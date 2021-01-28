cddd3cf1902d2ed86888b042ea86e2e9db12580f
Eduardo R <link2755@hotmail.com>
2021-01-27 21:34:35 -0300

    correction to submit on popup

--- script.js
+++ script.js
@@ -16,13 +16,17 @@ function addNewBook(){
     const author = document.getElementById("author").value;
     const pagesNumber = document.getElementById("pages").value;
     const imageUrl = document.getElementById("book-img").value;
-    const isRead = (document.getElementById("is-read").checked);
-    console.log(isRead);
+    const isRead = document.getElementById("is-read");
 
-    const book = new Book(title, author, pagesNumber, imageUrl, isRead);
+    const book = new Book(title, author, pagesNumber, imageUrl, isRead.checked);
     addBookToLibrary(book);
     
     //reseting forms and closing popup
+    const form = document.querySelector("form");
+    form.reset();
+    
+    //close the modal in submition
+    modal.style.display = "none";
 
 }
 
@@ -107,13 +111,13 @@ addBookToLibrary(zeze);
 
 //Modal
 // Get the modal
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("new-book-button");
+const btn = document.getElementById("new-book-button");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -123,13 +127,6 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
 }
 
 
